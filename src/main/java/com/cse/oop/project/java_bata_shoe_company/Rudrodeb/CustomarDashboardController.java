56b64dd33a1c702c906386bd07cd447b36d2b4e2
rudrodebsarker <rudrodebsarker@gmail.com>
2024-12-08 20:46:49 +0600

    Second Commit

---
+++ src/main/java/com/cse/oop/project/java_bata_shoe_company/Rudrodeb/CustomarDashboardController.java
@@ -0,0 +1,61 @@
+package com.cse.oop.project.java_bata_shoe_company.Rudrodeb;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CustomarDashboardController
+{
+    @javafx.fxml.FXML
+    private ListView checkNotifications;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextField searchBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void searchOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchPromotionOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchReadReviewOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogoutOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchCartOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeWindowOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchShoeModelsOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleUserProfileOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchSubmitReviewOnButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void switchOrderOnButtonClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.cse.oop.project.java_bata_shoe_company {
 
     opens com.cse.oop.project.java_bata_shoe_company to javafx.fxml;
     exports com.cse.oop.project.java_bata_shoe_company;
+    exports com.cse.oop.project.java_bata_shoe_company.Rudrodeb;
+    opens com.cse.oop.project.java_bata_shoe_company.Rudrodeb to javafx.fxml;
 }
