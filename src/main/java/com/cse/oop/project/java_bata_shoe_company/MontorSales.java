6c4c02c61251e9c199c9e311dc89babbabd6054f
DELL <Nafisfuad7@gmail.com>
2024-12-13 20:23:34 +0600

    new fx

---
+++ src/main/java/com/cse/oop/project/java_bata_shoe_company/MontorSales.java
@@ -0,0 +1,69 @@
+package com.cse.oop.project.java_bata_shoe_company;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.awt.TextArea;
+import java.awt.TextField;
+
+public class MontorSales
+{
+    @javafx.fxml.FXML
+    private Label dailyTargetNotMetLable;
+    @javafx.fxml.FXML
+    private TableColumn PerItemPriceTableColumn;
+    @javafx.fxml.FXML
+    private Label dailyTargetMetLable11;
+    @javafx.fxml.FXML
+    private TextField quantutyTextField;
+    @javafx.fxml.FXML
+    private Label warningLable;
+    @javafx.fxml.FXML
+    private TableColumn branchNameTableColumn;
+    @javafx.fxml.FXML
+    private TableView salesDataTableColumn;
+    @javafx.fxml.FXML
+    private TextArea summerizeSalesTextField;
+    @javafx.fxml.FXML
+    private TableColumn dailyTargetTableColumn;
+    @javafx.fxml.FXML
+    private TextArea showAllBranchweeklySalesTextField;
+    @javafx.fxml.FXML
+    private ComboBox nameOfBranchComboBox;
+    @javafx.fxml.FXML
+    private TableColumn dosTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn quantityTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox nameOfItemTextField;
+    @javafx.fxml.FXML
+    private TableColumn totalSalesTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn iteamNameTableColumn;
+    @javafx.fxml.FXML
+    private TextField itemPriceTextField;
+    @javafx.fxml.FXML
+    private DatePicker dosDatePicker;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void showallBranchesWeeklySalesButtononClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void summerizeSalesButtononClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exitButtonOnCLick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtononClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.cse.oop.project.java_bata_shoe_company {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.cse.oop.project.java_bata_shoe_company to javafx.fxml;
