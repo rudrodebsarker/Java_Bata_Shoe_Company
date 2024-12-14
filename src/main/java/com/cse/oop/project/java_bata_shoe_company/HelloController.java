583f7debf269db9ac5dcd99ebb2d248f80bfb70d
Saifahammad1 <ahammadsaif@gmail.com>
2024-12-15 03:55:49 +0600

    FXML updates

--- src/main/java/com/cse/oop/project/java_bata_shoe_company/HelloController.java
+++ src/main/java/com/cse/oop/project/java_bata_shoe_company/HelloController.java
@@ -5,13 +5,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-
     @FXML
     private Label welcomeText;
-
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-
 }
